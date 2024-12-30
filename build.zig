9eb4d14329c673c7f739b1cbb2e782ae3a1940f0
vinybrasil <vinicyus.brasil@baritecnologia.com.br>
2024-12-30 19:08:58 -0300

    breaking change: new layout

---
+++ build.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+const zine = @import("zine");
+
+pub fn build(b: *std.Build) !void {
+    zine.website(b, .{
+        .title = "Loris Cro's Blog",
+        .host_url = "https://kristoff.it",
+        .layouts_dir_path = "layouts",
+        .content_dir_path = "content",
+        .assets_dir_path = "assets",
+        .static_assets = &.{
+            "favicon.ico",
+            "CNAME",
+            ".well-known/atproto-did",
+        },
+        .debug = true,
+    });
+}
